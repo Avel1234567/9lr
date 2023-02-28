cd850f9512995e52e067714b4b7531a4b68f2e4b
Avel1234567 <103198769+Avel1234567@users.noreply.github.com>
2023-02-28 13:10:54 +0300

    added files

---
+++ Assets/Scrips/Coins.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*jgrej*/
+public class Coins : MonoBehaviour
+{
+    public static int Coin;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.CompareTag("Player"))
+        {
+            Coin += 1;
+            Destroy(gameObject);
+        }
+    }
+}
